bd38b96e04c6502386992cd14827f4f8a91cbaa5
Siddhant Chimankar <50448021+siddhantchimankar@users.noreply.github.com>
2019-12-09 20:43:55 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -10,28 +10,114 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     backgroundColor: Colors.indigo[100],
       appBar : AppBar(
-        title : Text('Fast-Cart'),
-        centerTitle : false,
-        backgroundColor: Colors.red[600],
+        title : Center(child: Text('Dashboard')),
+        backgroundColor: Colors.orangeAccent,
       ),
-      body : Center(
-        child : RaisedButton.icon(
-          onPressed: () {},
-          icon : Icon(
-              Icons.add_box
-          ),
-          label: Text('View Cart'),
-          color : Colors.amber,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 35, 30, 0),
+        child: Column(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+
+                    Center(
+                      child: CircleAvatar(
+                        backgroundColor: Colors.black,
+                        radius: 42.0,
+                        foregroundColor: Colors.black,
+
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(7.0),
+                      child: Center(
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('assets/duk.jpg'),
+                          backgroundColor: Colors.black,
+                          radius: 35.0,
+                          foregroundColor: Colors.black,
+
+                        ),
+                      ),
+
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0,),
+                Divider(
+                  height: 30.0,
+                  color: Colors.black,
+                ),
+                SizedBox(height: 10.0,),
+
+
+                Text(
+                  'NIK',
+                  style: TextStyle(
+                    color: Colors.red,
+                    letterSpacing: 2.0,
+                   fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(height: 5.0,),
+                Text(
+                  'Ash Ketchum',
+                  style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 26.0,
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  'REGION',
+                  style: TextStyle(
+                    color: Colors.red,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(height: 5.0,),
+                Text(
+                  'Pallet Town, Kanto',
+                  style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 26.0,
+                  ),
+                ),
+
+              ],
+            ),
+          ],
         ),
+
+
+
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed : () {},
-        child : Text('New'),
-        backgroundColor: Colors.red[600],
+        focusColor: Colors.orange,
+        hoverColor: Colors.red,
+        backgroundColor: Colors.orange,
+        child: Text('New'),
+
       ),
     );
   }
 }
+
 
 
